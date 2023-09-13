11ebb97adbe75e66eae3f6dc44e65c4e486b77eb
marcobusacca <marcobusacca01@gmail.com>
2023-09-13 17:25:43 +0200

    [FEAT] relation one-to-one restaurant and user

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -6,11 +6,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
+use App\Models\User;
+
 class Restaurant extends Model
 {
     use HasFactory;
 
     public static function generateSlug($name){
         return Str::slug($name, '-');
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
     }
 }
