3618704f15120f4fe3a6898b96a477f4ad1caee7
marcobusacca <59514839+marcobusacca@users.noreply.github.com>
2023-09-13 10:55:59 +0200

    Initial commit

---
+++ resources/views/mails/new_email_contact.blade.php
@@ -0,0 +1,9 @@
+<h1>Nuovo Contatto Ricevuto da Form</h1>
+<p>
+    Hai ricevuto un nuovo messaggio:<br><br>
+
+    Utente: {{ $lead->name }}<br><br>
+    Email: {{ $lead->email }}<br><br>
+    Contenuto: <br>
+    {{ $lead->content }}
+</p>
