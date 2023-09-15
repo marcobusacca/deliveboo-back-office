44c8de6b1c9a9c788ee961b4ea591f3465dfff10
alb1994 <antonetti_alberto@libero.it>
2023-09-15 16:27:18 +0200

    show pronto

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -1,8 +1,29 @@
-@extends('layouts.app')
+@extends('layouts.app') <!-- Estende il layout 'app' -->
 
 @section('content')
-    <div class="container py-5">
-        <div class="row">
+<div class="container py-5">
+    <div class="row">
+        <!-- Link per tornare alla lista dei prodotti -->
+        <div class="col-12 my-4">
+            <a href="{{ route('admin.products.index') }}" class="btn btn-dark rounded-5 mx-3">
+                <i class="fa-solid fa-arrow-left"></i>
+            </a>
         </div>
+
+        @if (empty($product->cover_image))
+            <!-- Immagine di sostituzione -->
+            <img class="card-img-top" src="{{ Vite::asset('resources/img/placeholder-image.jpg') }}" alt="{{ $product->slug }}-immagine-di-sostituzione">
+        @else
+            <!-- Immagine di copertina -->
+            <img class="card-img-top" src="{{ asset('storage/'.$product->cover_image) }}" alt="{{ $product->slug }}-immagine-di-copertina">
+        @endif
+
+        <!-- Visualizzazione delle informazioni del prodotto -->
+        <div>Nome: {{ $product->name }}</div>
+        <div>Ingredienti: {{ $product->ingredients }}</div>
+        <div>Prezzo: {{ $product->price }}</div>
+        <div>Descrizione: {{ $product->description }}</div>
+        <div>Disponibilità: {{ $product->visible == 1 ? 'si' : 'no' }}</div>
     </div>
+</div>
 @endsection
