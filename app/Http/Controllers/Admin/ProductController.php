a968c92f59b055449ba8dd3650ead9752e5405fc
marcobusacca <marcobusacca01@gmail.com>
2023-09-15 14:52:13 +0200

    [FEAT] restaurants products function show

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -45,7 +45,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return view('admin.products.show', compact('product'));
     }
 
     /**
