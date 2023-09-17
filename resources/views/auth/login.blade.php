926c916eceac89db1f43493812051ed309967095
andreathepa <andreadepa32@gmail.com>
2023-09-17 12:53:38 +0200

    [FEAT] Styling login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-4">
+<div class="container mt-5">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Effettua login</div>
+        <div class="col-12 col-md-8 col-lg-4">
+            <div class="card p-3">
+                <div class="card-header deliv-orange text-white text-center">Effettua login</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -14,7 +14,7 @@
                         <div class="mb-4 row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">E-mail</label>
 
-                            <div class="col-md-6">
+                            <div class="col-12">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -28,7 +28,7 @@
                         <div class="mb-4 row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-12 mb-4">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -40,28 +40,28 @@
                         </div>
 
                         <div class="mb-4 row">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-8 mb-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Resta Connesso') }}
+                                        Resta connesso
                                     </label>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="mb-4 row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
+                            <div class="col-12 mb-4">
+                                <div class="form-submit">
+                                    <button type="submit" class="btn btn-success button-color w-100">
+                                        Login
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="col-12 text-center">
                                 @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Password Dimenticata?') }}
-                                </a>
-                                @endif
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        Password dimenticata?
+                                    </a>
+                                    @endif
                             </div>
                         </div>
                     </form>
