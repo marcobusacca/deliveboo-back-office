ce9f488b128622d9418f06e2d1732893fd4e4707
marcobusacca <marcobusacca01@gmail.com>
2023-09-18 11:40:27 +0200

    [FIX] layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,51 +1,42 @@
 @extends('layouts.app')
+
 @section('content')
-
-<div class="jumbotron p-5 mb-4 rounded-3">
-    <div class="container card py-5 shadow-lg p-3 mb-5 bg-body-tertiary rounded">
-        <div class="row">
-            <div class="col-12">
-                <div class="text-center p-3">
-                    <h1 class="mb-3">Benvenuto nella tua pagina personale Deliveboo</h1>
-                    <h4 class="mb-3">Unisciti alla comunity di Deliveboo</h4>
-                    <p class="">Insieme possiamo aiutarti a raggiungere sempre più clienti</p>
+    <div class="jumbotron p-5 mb-4 rounded-3">
+        <div class="container card py-5 shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+            <div class="row">
+                <div class="col-12">
+                    <div class="text-center p-3">
+                        <h1 class="mb-3">Benvenuto nella piattaforma ristoranti Deliveboo</h1>
+                        <h4 class="mb-3">Unisciti alla comunity di Deliveboo</h4>
+                        <p class="">Insieme possiamo aiutarti a raggiungere sempre più clienti!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container card mt-5 p-4 shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+            <div class="row align-items-center">
+                <div class="text-center">
+                    <h1 class="mb-5">Cosa puoi fare</h1>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="text-center">
+                        <i class="fa-solid fa-circle-info fa-xl mb-4"></i>
+                        <p>Gestire il tuo ristorante e i tuoi prodotti</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="text-center">
+                        <i class="fa-solid fa-receipt fa-2xl mb-4"></i>
+                        <p>Controllare le ordinazioni in maniera efficiente</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                    <div class="text-center">
+                        <i class="fa-solid fa-ranking-star fa-2xl mb-4"></i>
+                        <p>Visionare le statistiche dei tuoi ordini</p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <div class="container card mt-5 p-4 shadow-lg p-3 mb-5 bg-body-tertiary rounded">
-        <div class="row align-items-center">
-            <div class="text-center">
-                <h1 class="mb-5">Cosa puoi fare</h1>
-            </div>
-            <div class="col-12 col-md-6 col-lg-4">
-                <div class="text-center">
-                    <i class="fa-solid fa-circle-info fa-xl mb-4"></i>
-                    <p>Gestire il tuo ristorante e i tuoi prodotti</p>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-4">
-                <div class="text-center">
-                    <i class="fa-solid fa-receipt fa-2xl mb-4"></i>
-                    <p>Controllare le ordinazioni in maniera efficiente</p>
-                </div>
-            </div>
-            <div class="col-12 col-md-6 col-lg-4">
-                <div class="text-center">
-                    <i class="fa-solid fa-ranking-star fa-2xl mb-4"></i>
-                    <p>Visionare le statistiche dei tuoi ordini</p>
-                </div>
-            </div>
-            <div class="text-center">
-                <a href="{{ url('admin') }}">
-                    <button type="button" class="btn btn-primary mt-4">Accedi alla tua dashboard</button>
-                </a>
-            </div>
-        </div>
-    </div>
-
-</div>
-
-
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,33 +44,33 @@
                     <ul class="navbar-nav ml-auto text-white">
                         <!-- Authentication Links -->
                         @guest
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Accedi') }}</a>
+                            </li>
                         @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="{{ route('register') }}">Registrati</a>
-                        </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="{{ route('register') }}">{{ __('Registrati') }}</a>
+                            </li>
                         @endif
                         @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ url('admin') }}">{{__('Dashboard')}}</a>
-                                <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ url('admin') }}">{{__('Dashboard')}}</a>
+                                    <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
                         @endguest
                     </ul>
                 </div>
