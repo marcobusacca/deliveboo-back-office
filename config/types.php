72a86673b994981f5757360e3dffdac148ec3389
antoniogisondi <anto.gisondi@tiscali.it>
2023-09-19 17:17:20 +0200

    [FEAT] API Types done and seeder

--- config/types.php
+++ config/types.php
@@ -3,46 +3,46 @@
 return [
     [
         'name'          =>      'Trattoria',
-        'cover_image'   =>      'types_images/trattoria.jpg',
+        'cover_image'   =>      'types_images/trattoria.png',
     ],
     [
         'name'          =>      'Messicano',
-        'cover_image'   =>      'types_images/messicano.jpg',
+        'cover_image'   =>      'types_images/messicano.png',
     ],
     [
         'name'          =>      'Pizzeria',
-        'cover_image'   =>      'types_images/pizzeria.jpg',
+        'cover_image'   =>      'types_images/pizzeria.png',
     ],
     [
         'name'          =>      'Steakhouse',
-        'cover_image'   =>      'types_images/steakhouse.jpg',
+        'cover_image'   =>      'types_images/steakhouse.png',
     ],
     [
         'name'          =>      'Cinese',
-        'cover_image'   =>      'types_images/cinese.jpg',
+        'cover_image'   =>      'types_images/cinese.png',
     ],
     [
         'name'          =>      'Sushi',
-        'cover_image'   =>      'types_images/sushi.jpg',
+        'cover_image'   =>      'types_images/sushi.png',
     ],
     [
         'name'          =>      'Fast Food',
-        'cover_image'   =>      'types_images/fast-food.jpg',
+        'cover_image'   =>      'types_images/fast-food.png',
     ],
     [
         'name'          =>      'Gelateria',
-        'cover_image'   =>      'types_images/gelateria.jpg',
+        'cover_image'   =>      'types_images/gelateria.png',
     ],
     [
         'name'          =>      'Kebab',
-        'cover_image'   =>      'types_images/kebab.jpg',
+        'cover_image'   =>      'types_images/kebab.png',
     ],
     [
         'name'          =>      'Pasticceria',
-        'cover_image'   =>      'types_images/pasticceria.jpg',
+        'cover_image'   =>      'types_images/pasticceria.png',
     ],
     [
         'name'          =>      'Piadineria',
-        'cover_image'   =>      'types_images/piadineria.jpg',
+        'cover_image'   =>      'types_images/piadineria.png',
     ],
 ];
