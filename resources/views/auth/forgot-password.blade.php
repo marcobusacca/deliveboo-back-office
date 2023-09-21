ad6173d0cd627e37f91ff61512f31417e22fa4b6
marcobusacca <marcobusacca01@gmail.com>
2023-09-21 16:07:15 +0200

    [STYLE] forgot-password view style

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mt-4">
+<div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reimposta Password') }}</div>
+            <div class="card shadow-lg bg-body-tertiary rounded">
+                <div class="card-header deliv-orange text-white text-center">{{ __('Reimposta Password') }}</div>
                 <div class="card-body">
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
