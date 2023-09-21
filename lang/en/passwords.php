244926d194bcd36e1985740227b60d2a1c090059
marcobusacca <marcobusacca01@gmail.com>
2023-09-21 11:45:53 +0200

    [FEAT] login e register error message translate

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'La tua password è stata reimpostata!',
+    'sent' => 'Abbiamo inviato il link per il reset della password via email!',
+    'throttled' => 'Attendi prima di riprovare.',
+    'token' => 'Questo token per il reset della password non è valido.',
+    'user' => "Non riusciamo a trovare un utente con questo indirizzo email.",
 
 ];
