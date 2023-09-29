b94016990eee1fbfe7897f38371f4936e9a12f30
antoniogisondi <anto.gisondi@tiscali.it>
2023-09-29 18:21:40 +0200

    FIX Checkout

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -11,8 +11,38 @@ use App\Models\Order;
 
 class PaymentController extends Controller
 {
+    protected $braintreeService;
+
+    public function __construct(BrainTree $braintreeService)
+    {
+        $this->braintreeService = $braintreeService;
+    }
+
     public function processPayment(Request $request){
+        $amount = $request->all();
+        $cardNumber = $request->input('card_number');
+        $expiryDate = $request->input('expiry_date');
+        $cvv = $request->input('cvv');
+
+        $result = $this->braintreeService::sale([
+            'amount' => $amount,
+            'creditCard' => [
+                'card_number' => $cardNumber,
+                'expiry_date' => $expiryDate,
+                'cvv' => $cvv
+            ]
+        ]);
         
+        if ($result->success) {
+            // Il pagamento è andato a buon fine
+            $transaction = $result->transaction;
+        } else {
+            // C'è stato un problema con il pagamento
+            $errorString = "";
+            foreach($result->errors->deepAll() as $error) {
+                $errorString .= 'Errore: ' . $error->code . ": " . $error->message . "\n";
+            }
+        }
     }
 }
 
